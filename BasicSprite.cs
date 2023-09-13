c102502c84ccf0a658e064ab92486d14d6d7f1f7
Mylan Li <mylanli@hotmail.com>
2023-09-13 23:47:45 +1000

    setup for basic sprites, recording plan

---
+++ BasicSprite.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectOne;
+
+public class BasicSprite {
+
+    public BasicSprite() {
+        //pass in the texture reference? or maybe just a string of the colour name, read it to use when generating?
+
+        //details to hold, location?
+    }
+
+    //methods to adjust the properties?
+
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Hello, I'm learning more about programming by playing with Monogame.
 
-And also to get used to using git as well as trying to add this to my routine \)
+And also to get used to using git as well as trying to add this to my routine
+
+Current goals:
+- generating objects
+- clicking on objects and the method chains with them
+
+- once this is done, clean up experiemented on code
